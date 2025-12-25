94ee27ea00e1ae47e16fb23756f4b7d075d370e5
Ksawex <ksawex17@gmail.com>
2025-12-25 20:00:11 +0100

    Added a few error checks

--- scripts/GamesManager.gd
+++ scripts/GamesManager.gd
@@ -1,26 +1,34 @@
 extends Node
 # This scripts gets data for the games from the repo
+# getting certain release string: "/releases/tag/%s" % "v" + "0.1.0"
 
 var Http_requester := HTTPRequest.new()
 var Games: Dictionary = {}
 signal UpdateGames()
 
-"/releases/tag/%s"
+
 func _ready() -> void:
 	add_child(Http_requester)
 	Http_requester.request_completed.connect(self._request_complete)
 	var err := Http_requester.request("http://localhost:12345/games.json")
-	print("Fuck")
 	if err != OK:
 		print("error with http request: ", err)
 
 
-func _request_complete(_result, _response_code, _headers, body) -> void:
-	print("got file")
+func _request_complete(result, response_code, _headers, body) -> void:
+	if result != OK:
+		print("result is ", result)
+		return
+	if response_code != 200:
+		print("response code ", response_code)
+		return
 	var json = JSON.new()
-	json.parse(body.get_string_from_utf8())
+	var err := json.parse(body.get_string_from_utf8())
+	if err != OK:
+		print("Failed to parse: ", err)
+		return
 	var response = json.get_data()
 	
-	print(response)
-	Games = response
-	UpdateGames.emit()
+	if response is Dictionary:
+		Games = response
+		UpdateGames.emit()
